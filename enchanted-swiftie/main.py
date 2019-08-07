db684530c9971958421e1252c0f3faed08ae34fa
Hadar Dagan <hadarda11@gmail.com>
2019-08-07 13:20:43 +0300

    added on/off option for all individual features

--- enchanted-swiftie/main.py
+++ enchanted-swiftie/main.py
@@ -3,66 +3,92 @@ from discord.ext import commands
 import random
 from google_images_search import GoogleImagesSearch
 
+# Stores the feature current status - True for on False for off
+tayball = True
+event_status = True
 
 client = commands.Bot(command_prefix="t!")
 with open("google shit.txt", "rt") as codes:
     gis = GoogleImagesSearch(codes.readline().rstrip(), codes.readline().strip())
 
+
 # TODO move this to chat:
 @client.command(aliases=["Tayball", "tayball", "tayBall", "TayBall"])
 async def tay_ball(ctx, *, question):
     """Sends Taylorized 8ball answers"""
-    # Only exists for the New Romantics/Wiledst Dreams test
-    question = question.lower()
-    # Taylorized 8ball answers, 10 positive, 5 neutral, 5 negative
-    answers = ["Taylor thinks it is certain",
-               "Taylor has no doubt",
-               "Taylor will rely on it",
-               "As taylor sees it, yes",
-               "Taylor says it's most likely",
-               "Taylor's outlook is good",
-               "Leaks point to yes",
-               "Taylor's positive that is the case",
-               "Taylor's sure of it",
-               "Taylor thinks it's definite",
-               "Taylor's hazy, try again",
-               "Ask Taylor later",
-               "Taylor will tell you in a different time",
-               "Taylor can't predict now",
-               "Concentrate and ask again",
-               "Taylor won't count on it",
-               "Taylor's reply is no",
-               "Leaks say no",
-               "Taylor's outlook isn't very good",
-               "Taylor is very doubtful"]
-    selected_message = random.randint(0, len(answers) - 1)
-    # Providing a positive answer if someone asks if New Romantics/Wildest Dreams are good songs
-    if question.find("new romantics") != -1 or question.find("wildest dreams") != -1:
-        if question.find("best") != -1 or question.find("good") != -1 \
-                or question.find("fantastic") != -1 or question.find("great") != -1:
-                selected_message = random.randint(0, 9)
-        elif question.find("worst") != -1 or question.find("bad") != -1 \
-                or question.find("trash") != -1 or question.find("garbage") != -1:
-                selected_message = random.randint(14, len(answers) - 1)
-    await ctx.send(answers[selected_message])
+    global tayball
+    if question == "off":
+        tayball = False
+        await ctx.send("Tayball feature is now turned off")
+    if question == "on":
+        tayball = True
+        await ctx.send("Tayball feature is now turned on")
+        return
+    if tayball:
+        # Only exists for the New Romantics/Wiledst Dreams test
+        question = question.lower()
+        # Taylorized 8ball answers, 10 positive, 5 neutral, 5 negative
+        answers = ["Taylor thinks it is certain",
+                   "Taylor has no doubt",
+                   "Taylor will rely on it",
+                   "As taylor sees it, yes",
+                   "Taylor says it's most likely",
+                   "Taylor's outlook is good",
+                   "Leaks point to yes",
+                   "Taylor's positive that is the case",
+                   "Taylor's sure of it",
+                   "Taylor thinks it's definite",
+                   "Taylor's hazy, try again",
+                   "Ask Taylor later",
+                   "Taylor will tell you in a different time",
+                   "Taylor can't predict now",
+                   "Concentrate and ask again",
+                   "Taylor won't count on it",
+                   "Taylor's reply is no",
+                   "Leaks say no",
+                   "Taylor's outlook isn't very good",
+                   "Taylor is very doubtful"]
+        selected_message = random.randint(0, len(answers) - 1)
+        # Providing a positive answer if someone asks if New Romantics/Wildest Dreams are good songs
+        if question.find("new romantics") != -1 or question.find("wildest dreams") != -1:
+            if question.find("best") != -1 or question.find("good") != -1 \
+                    or question.find("fantastic") != -1 or question.find("great") != -1:
+                    selected_message = random.randint(0, 9)
+            elif question.find("worst") != -1 or question.find("bad") != -1 \
+                    or question.find("trash") != -1 or question.find("garbage") != -1:
+                    selected_message = random.randint(14, len(answers) - 1)
+        await ctx.send(answers[selected_message])
 
 
 @client.command(aliases=["Event"])
 async def event(ctx, *, event_name=""):
     """sends either a random image of taylor swift or the first image on google images from a given query"""
-    # Creates a proper search query
-    query = "Taylor Swift " + event_name
-    if event_name.strip() == "":
-        query = "Taylor Swift " + random.choice("123456789") + random.choice("123456789") + \
-                random.choice("123456789")
-    try:
-        await ctx.send(gis.search(search_params={'q': query, 'num': 1}))
-        # TODO check what is the failure error
-    #except FALIUREERROR:
-        #await ctx.send("sorry, try again")
-    except:
-        await ctx.send("sorry, try again tomorrow")
-
+    global event_status
+    # Turns the feature on or off
+    if event_name == "off":
+        event_status = False
+        await ctx.send("Image feature is now turned off")
+    if event_name == "on":
+        event_status = True
+        await ctx.send("Image feature is now turned on")
+        return
+    if event_status:
+        # Creates a proper search query
+        query = "Taylor Swift " + event_name
+        if event_name.strip() == "":
+            query = "Taylor Swift " + random.choice("123456789") + random.choice("123456789") + \
+                    random.choice("123456789")
+        try:
+            url = gis.search(search_params={'q': query, 'num': 1})
+            await ctx.send(url)
+        except Exception as e:
+            # User quota filled so google won't provide a url, error type doesn't exist
+            # So have to manually search for error message
+            if str(e).find("400 BAD REQUEST") != -1 or str(e).find("HttpError") != -1:
+                await ctx.send("User image quota filled, please wait a minute and try again")
+            # Daily quota filled, google won't provide more urls
+            else:
+                await ctx.send("sorry, try again tomorrow")
 
 
 def main():
